fa14af36697cd703cb95b0fa61a2361cad619961
Jose Palenzuela <jpalenz@gmail.com>
2025-11-04 09:51:12 +0100

    Add README

--- service.fel_mel_notification/service.py
+++ service.fel_mel_notification/service.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 
 # Ruta al script bash
-SCRIPT_PATH = "/storage/.kodi/userdata/fel_mel_top_left.sh"
+SCRIPT_PATH = "/storage/.kodi/userdata/fel_mel_notification.sh"
 
 while not xbmc.Monitor().abortRequested():
     # Espera a que un reproductor inicie
